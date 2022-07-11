b6928842de9c09036e5be029cccc6a9bb91f590c
Hammad Ali <hammad.a22002@gmail.com>
2022-07-11 16:48:09 +0500

    added signUp function with JWT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios";
+import { SERVER_URL } from "./config";
+
+axios.defaults.baseURL = SERVER_URL;
+const authtoken = localStorage.getItem("authtoken");
+console.log("Authotoken is " + authtoken);
+axios.defaults.headers.common["Authorization"] = `Bearer ${authtoken}`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
