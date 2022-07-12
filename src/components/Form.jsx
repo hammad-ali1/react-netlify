5cf168635dcd86ddccc332d5982b658855dbe001
Hammad Ali <hammad.a22002@gmail.com>
2022-07-12 13:19:52 +0500

    add title in todo form

---
+++ src/components/Form.jsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TextField } from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Form({
+  open,
+  handleClose,
+  data,
+  onChange,
+  handleFormSubmit,
+  title,
+  buttonText,
+  formFields,
+}) {
+  const { C_ID, C_name, city, address, capacity, M_ID } = data;
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <form>
+            {formFields.map((field, key) => {
+              return (
+                <TextField
+                  key={key}
+                  id={field.id}
+                  value={field.value}
+                  onChange={(event) => {
+                    onChange(event);
+                  }}
+                  placeholder={field.placeholder}
+                  label={field.label}
+                  fullWidth
+                  margin="dense"
+                />
+              );
+            })}
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" color="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleFormSubmit();
+            }}
+          >
+            {buttonText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ src/styles/AddButton.styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 5px;
+  margin: auto;
+  display: flex;
+  button {
+    background-color: #0a0d175d;
+    /* background-color: #00bfff; */
+    margin: auto;
+    border-radius: 50%;
+    border: none;
+    height: 50px;
+    width: 50px;
+    font-size: 50px;
+    line-height: 50px;
+    color: #cdbbbb;
+    box-shadow: 2px 5px 4px #271d1d;
+  }
+`;
