d0664c0a83965cd4765f068813d286406f3a327d
Hammad Ali <hammad.a22002@gmail.com>
2022-07-12 23:52:54 +0500

    small change

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
+import io from "socket.io-client";
 import { SERVER_URL } from "./config";
 
 axios.defaults.baseURL = `${SERVER_URL}/api`;
+io(SERVER_URL, { transports: ["websocket"] });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
