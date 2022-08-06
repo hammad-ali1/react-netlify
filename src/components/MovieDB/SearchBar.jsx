b40d0a098234fd679be0ab079128030bb74a86c7
Hammad Ali <hammad.a22002@gmail.com>
2022-08-06 21:21:36 +0500

    Logic of searching movie in useHomeFetch hook

--- src/components/MovieDB/SearchBar.jsx
+++ src/components/MovieDB/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 //Icons
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,8 +6,25 @@ import SearchIcon from "@mui/icons-material/Search";
 //Styles
 import { Wrapper, Content } from "../../styles/MovieDB/SearchBar.styles";
 
+const TIME_DELAY_OF_SEARCH = 5000;
 function SearchBar({ setSearchTerm }) {
+  //state
   const [state, setState] = useState("");
+  //refs
+  const initialRender = useRef(true);
+  //effects
+  useEffect(() => {
+    if (initialRender.current) {
+      //don't setSearch Term on initial render
+      initialRender.current = false;
+      return;
+    }
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, TIME_DELAY_OF_SEARCH);
+
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
   return (
     <Wrapper>
       <Content>
