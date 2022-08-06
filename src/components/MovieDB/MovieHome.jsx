e377caf751a7cb2de194a2b1607ee49fc70ac985
Hammad Ali <hammad.a22002@gmail.com>
2022-08-06 18:33:31 +0500

    Added useEffect hook in MovieHome

--- src/components/MovieDB/MovieHome.jsx
+++ src/components/MovieDB/MovieHome.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+//API
+import API from "../../api/movie.api";
 //Styles
 import { Wrapper } from "../../styles/MovieDB/MovieHome.styles";
 
@@ -10,9 +12,35 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../../config";
 
 //Image
 function MovieHome() {
+  //states
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  //effects
+  //initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+  //functions
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
 
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState((prevState) => ({
+        ...movies,
+        results:
+          page > 1
+            ? [...prevState.results, ...movies.results]
+            : [...movies.results],
+      }));
+    } catch (err) {
+      setError(true);
+    }
+    setLoading(false);
+  };
   return <Wrapper>Start Here</Wrapper>;
 }
 
