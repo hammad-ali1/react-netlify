29c8aa2ed08558069237e039f17a83afb1c0efc7
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 00:53:47 +0500

    Movie component structure

---
+++ src/components/MovieDB/Movie.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Styles
+import { Wrapper } from "../../styles/MovieDB/Movie.style";
+
+function Movie() {
+  return <Wrapper>Movie</Wrapper>;
+}
+
+export default Movie;
