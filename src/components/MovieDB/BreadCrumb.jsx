7e604411f376732f5ce28b3f329338a0bed39cc8
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 13:11:54 +0500

    structure of BreadCrumb

---
+++ src/components/MovieDB/BreadCrumb.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper, Content } from "../../styles/MovieDB/BreadCrumb.styles";
+
+function BreadCrumb() {
+  return (
+    <Wrapper>
+      <Content></Content>
+    </Wrapper>
+  );
+}
+
+export default BreadCrumb;
