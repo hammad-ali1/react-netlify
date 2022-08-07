37d69fced0d234a8640a1a178a3ae6b7cf88afb5
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 15:07:58 +0500

    Feature MovieDB merge

---
+++ src/components/MovieDB/Movie.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+//Styles
+import { Wrapper } from "../../styles/MovieDB/Movie.styles";
+//Components
+import BreadCrumb from "./BreadCrumb";
+import Grid from "./Grid";
+import Spinner from "../Spinner";
+//Hook
+import { useMovieFetch } from "../../hooks/MovieDB/useMovieFetch";
+
+//Image
+import NO_IMAGE from "../../images/no_image.jpg";
+import MovieInfo from "./MovieInfo";
+function Movie() {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  if (loading) return <Spinner />;
+  return (
+    <Wrapper>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+    </Wrapper>
+  );
+}
+
+export default Movie;
