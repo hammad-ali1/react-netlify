fccf0897bdbbdf844b7f2f2e19e11587545e875f
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 12:35:27 +0500

    clickable Thumb logic

--- src/components/MovieDB/Thumb.jsx
+++ src/components/MovieDB/Thumb.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 //styles
 import { Image } from "../../styles/MovieDB/Thumb.styles";
 function Thumb({ image, movieId, clickable }) {
+  const currentLocation = useLocation().pathname;
   return (
     <div>
-      <Image src={image} alt="movie-thumb"></Image>
+      {clickable ? (
+        <Link to={`${currentLocation}/movie/${movieId}`}>
+          <Image src={image} alt="movie-thumb"></Image>
+        </Link>
+      ) : (
+        <Image src={image} alt="movie-thumb"></Image>
+      )}
     </div>
   );
 }
