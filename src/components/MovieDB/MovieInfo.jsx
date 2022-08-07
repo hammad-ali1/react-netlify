ab0b545ee50fc47da492c209fc93f63eae6726af
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 16:53:33 +0500

    movie info styles

--- src/components/MovieDB/MovieInfo.jsx
+++ src/components/MovieDB/MovieInfo.jsx
@@ -14,7 +14,7 @@ import NO_IMAGE from "../../images/no_image.jpg";
 
 function MovieInfo({ movie }) {
   return (
-    <Wrapper>
+    <Wrapper backdrop={movie.backdrop_path}>
       <Content>
         <Thumb
           image={
@@ -24,7 +24,26 @@ function MovieInfo({ movie }) {
           }
           clickable={false}
         />
-        <Text></Text>
+        <Text>
+          <h1>{movie.title}</h1>
+          <h3>PLOT</h3>
+          <p>{movie.overview}</p>
+
+          <div className="rating-directors">
+            <div>
+              <h3>RATING</h3>
+              <div className="score">
+                {Number.parseFloat(movie.vote_average).toFixed(1)}
+              </div>
+            </div>
+            <div className="director">
+              <h3>DIRECTOR</h3>
+              {movie.directors.map((director) => (
+                <p key={director.credit_id}>{director.name} </p>
+              ))}
+            </div>
+          </div>
+        </Text>
       </Content>
     </Wrapper>
   );
