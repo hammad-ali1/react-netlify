60701d3850a9ba4fec50131fd37b5b1d5b985fd7
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 13:01:45 +0500

    useMovieFetch hook

--- src/components/MovieDB/Movie.jsx
+++ src/components/MovieDB/Movie.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 //Styles
 import { Wrapper } from "../../styles/MovieDB/Movie.styles";
 
+//Hook
+import { useMovieFetch } from "../../hooks/MovieDB/useMovieFetch";
+
+//Image
+import NO_IMAGE from "../../images/no_image.jpg";
 function Movie() {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
   return <Wrapper>Movie</Wrapper>;
 }
 
