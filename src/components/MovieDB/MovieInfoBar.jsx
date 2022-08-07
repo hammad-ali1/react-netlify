c82638e5e4e98b06fa2ff79abdceb9371e0f8583
Hammad Ali <hammad.a22002@gmail.com>
2022-08-07 16:58:44 +0500

    Movie Info Bar Structure

---
+++ src/components/MovieDB/MovieInfoBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//Styles
+import { Wrapper } from "../../styles/MovieDB/MovieInfoBar.styles";
+function MovieInfoBar() {
+  return <Wrapper></Wrapper>;
+}
+
+export default MovieInfoBar;
