a9dc7cfba767f1bb5b008d57b7c0c4a2cfee99f1
Hammad Ali <hammad.a22002@gmail.com>
2022-08-22 21:19:40 +0500

    remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import { useEffect, useState } from "react";
-import { getTeacherByName } from "./firebase/functions/teachers.handler";
-import { addUser } from "./firebase/auth/signup.auth";
 
-// addUser("hammadkhalid22002@gmail.com", "12345678");
 function App() {
-  const [teachers, setTeachers] = useState<any[]>([]);
-  useEffect(() => {
-    getTeacherByName("Sana").then(setTeachers);
-  }, []);
   return (
     <div className="App">
-      {teachers.map((teacher: any) => (
-        <div>{teacher.name}</div>
-      ))}
       <h1>Welcome to Hammad's React projects</h1>
     </div>
   );
