4b34eac5d7797b67cdface63df7cebcc188304cb
Hammad Ali <hammad.a22002@gmail.com>
2022-08-24 16:36:19 +0500

    review component

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import useEmployeeFetch from "../hooks/useEmployeeFetch";
 import Spinner from "../components/Spinner/Spinner";
 import EmployeeInfo from "../components/EmployeeInfo/EmployeeInfo";
+import Reviews from "../components/Reviews/Reviews";
 
 function Employee() {
   const { id } = useParams();
@@ -10,6 +11,7 @@ function Employee() {
     <div>
       {loading && <Spinner />}
       <EmployeeInfo employee={employee} />
+      <Reviews employee={employee} />
     </div>
   );
 }
