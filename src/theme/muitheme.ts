17f755dc39ae0e3f976335b72fb9532535020001
Hammad Ali <hammad.a22002@gmail.com>
2022-08-27 10:00:14 +0500

    Update muitheme.ts

--- src/theme/muitheme.ts
+++ src/theme/muitheme.ts
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material/styles";
-import { purple, orange } from "@mui/material/colors";
-
+import { orange, lightBlue } from "@mui/material/colors";
+import { Color } from "@mui/material";
 declare module "@mui/material/styles" {
   interface Theme extends ThemeOptions {}
   // allow configuration using `createTheme`
@@ -8,34 +8,40 @@ declare module "@mui/material/styles" {
     status: {
       danger: string;
     };
-    colors: {
-      white: string;
-    };
   }
 
   interface PaletteOptions {
     myColors: {
-      white: string;
+      [key: string]: Partial<Color>;
     };
+    tertiary: PaletteOptions["primary"];
   }
-  interface Palette extends PaletteOptions {}
+  interface Palette extends PaletteOptions {
+    tertiary: Palette["primary"];
+  }
 }
 
 const theme = createTheme({
   palette: {
     mode: "dark",
-    primary: purple,
+    tertiary: lightBlue,
     background: { default: "#202124" },
     myColors: {
-      white: "white",
+      lightGreen: {
+        "50": "#e8f8eb",
+        "100": "#c8edcf",
+        "200": "#a5e2b0",
+        "300": "#7ed790",
+        "400": "#5fcd77",
+        "500": "#3fc35e",
+        "600": "#35b354",
+        "700": "#29a148",
+      },
     },
   },
   //custom variables in theme
   status: {
     danger: orange[500],
-  },
-  colors: {
-    white: "white",
   },
 });
 export default theme;
